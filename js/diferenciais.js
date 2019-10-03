b5bf528a62e2bc1cc27dec6d67c63bf278eed78b
vitorregisr <vitorregisrr@gmail.com>
2019-10-03 18:43:37 -0300

    Add diferenciais slide navigation

--- js/diferenciais.js
+++ js/diferenciais.js
@@ -1,4 +1,25 @@
 (function () {
     'use strict';
    
+    $('.diferenciais__slide-actions .right').click( function(){
+        const nextImg = $(this).parent().parent()
+                        .find('img.active')
+                        .next();
+
+                        console.log(nextImg)
+
+        nextImg.siblings().removeClass('active');
+        nextImg.addClass('active');
+    });
+
+    $('.diferenciais__slide-actions .left').click( function(){
+        const prevImg = $(this).parent().parent()
+                        .find('img.active')
+                        .prev();
+
+                        console.log(prevImg)
+
+        prevImg.siblings().removeClass('active');
+        prevImg.addClass('active');
+    });
 })();
