f2ec0bc45194ad904f346203669098578398fc32
vitorregisr <vitorregisrr@gmail.com>
2019-10-03 11:48:30 -0300

    Add galeria

---
+++ js/galeria.js
@@ -0,0 +1,20 @@
+(function(){
+    'use strict';
+
+    const mainImage = $('#galeria-main-img');
+
+    $('.galeria__list-item').click( function(){
+        const newImgSrc = $(this).attr('data-open');
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+
+        $("html, body").animate({
+            scrollTop: mainImage.offset().top - 120
+        }, 700);
+
+        mainImage.fadeOut(300, function(){
+            mainImage.attr('src', newImgSrc);
+            mainImage.fadeIn(300);
+        });
+    })
+})();
