b16e201533f04714fc740e8bd66f4e3895acebe2
vitorregisr <vitorregisrr@gmail.com>
2019-10-04 11:31:44 -0300

    Add imoveis content and responsive

--- js/imoveis.js
+++ js/imoveis.js
@@ -7,13 +7,16 @@
         $(target).addClass('active');
         
         $("html, body").animate({
-            scrollTop: target.offset().top - 100
+            scrollTop: target.offset().top - 30
         }, 700);
     });
     
     $('.imoveis__slide-option').click( function(){
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+
         const target = $($(this).attr("data-toggle"));
-        $('.imoveis__slide-item').removeClass('active');
+        $(this).parent().parent().parent().find('.imoveis__slide-item').removeClass('active');
         $(target).addClass('active');
 
         $("html, body").animate({
